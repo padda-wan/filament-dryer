658fdc2579a289605ee47bbc8bffc47dea7236c2
Jason <jklaaste@gmail.com>
2021-08-12 09:15:52 +0200

    Added max temp to test

--- filament-dryer.ino
+++ filament-dryer.ino
@@ -2,18 +2,27 @@
 // https://drive.google.com/uc?export=download&id=1eG0a4lXQ2tNuTy_O9OWXuHO-EYbmHRDT
 
 #include <dht11.h>
+#include <math.h>
 
 int DHT11_pin=2;
 dht11 DHT11_sensor;
+float max_temp=0.0;
+float current_temp=0.0;
 
 void setup()
 {
   Serial.begin(9600);
+  max_temp = (float)DHT11_sensor.temperature;
 }
 
 void loop()
 {
   int chk = DHT11_sensor.read(DHT11_pin);
+
+  current_temp = (float)DHT11_sensor.temperature;
+  if (current_temp > max_temp){
+      max_temp = current_temp;
+  }
 
   Serial.print("Humidity (%): ");
   Serial.print((float)DHT11_sensor.humidity, 2);
@@ -25,6 +34,12 @@ void loop()
 
   Serial.print("Dew Point: ");
   Serial.print(DHT11_sensor.dewPoint(), 2);
+  Serial.print(" ");
+
+  Serial.print("Max Temp: ");
+  Serial.print(max_temp);
+  Serial.print(" ");
+
   Serial.println();
   Serial.println("");
   delay(500);
